ac01461e94f2e83cfa99ce738c74be8a55355f62
Cole DeVries <colemandevries@gmail.com>
2023-01-29 12:38:16 -0500

    created the alb resource module, variables, outputs

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "arn" {
+  value = aws_lb.this.arn
+}
+
+output "arn_suffix" {
+  value = aws_lb.this.arn_suffix
+}
+
+output "dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "id" {
+  value = aws_lb.this.id
+}
+
+output "name" {
+  value = aws_lb.this.name
+}
+
+output "security_groups" {
+  value = aws_lb.this.security_groups
+}
